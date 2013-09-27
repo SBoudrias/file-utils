eba83d32de3dc68d0e9ef93d215c9dc7bd2d8d3c
Simon Boudrias <simon.boudrias@cossette.com>
2013-09-27 16:50:02 -0400

    Initial commit (unit test not working yet) - broken

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+/*
+ * grunt
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      all: ['tests/file_test.js']
+    },
+    jshint: {
+      lib: ['lib/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: 'nofunc',
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-bump');
+
+  // "npm test" runs these tasks
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+
+  // Default task.
+  grunt.registerTask('default', ['test']);
+
+};
