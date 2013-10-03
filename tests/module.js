b62cc66674e755aa2e4a7e88091747b9abacb96a
Simon Boudrias <simon.boudrias@cossette.com>
2013-10-03 17:53:09 -0400

    Add environment creation test

--- tests/module.js
+++ tests/module.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var file = require('..');
+var File = require('../lib/file');
 var _ = require('lodash');
 
 exports['file module'] = {
@@ -17,5 +18,23 @@ exports['file module'] = {
         test.done();
       }
     }
-  )
+  ),
+  'createEnv': function(test) {
+    test.expect(6);
+    var logger = { bar: 'foo' };
+    var src = file.createEnv({
+      write: false,
+      encoding: 'usc2',
+      logger: logger,
+      base: 'src/'
+    });
+
+    test.equal(src.option('write'), false);
+    test.equal(src.option('encoding'), 'usc2');
+    test.equal(src.option('logger'), logger);
+    test.equal(src.option('base'), 'src/');
+    test.notEqual(src, file);
+    test.ok(src instanceof File);
+    test.done();
+  }
 };
