54e969344f51583794becf00248e99d3dc6b3a59
Simon Boudrias <simon.boudrias@cossette.com>
2013-10-04 15:15:21 -0400

    Rework modules binding to contains all in the File contructor

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var _ = require('lodash');
 var File = require('./lib/file');
 var Env = require('./lib/env');
-var query = require('./lib/query');
 
 var file = new File();
-_.extend(file, query);
-
-file.glob = require('glob');
-file.minimatch = require('minimatch');
-file.findup = require('findup-sync');
 
 file.createEnv = function(opt) {
   return new Env(opt);
